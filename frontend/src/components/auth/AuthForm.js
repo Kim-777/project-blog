9542bd4f43613649aae89c1106be5476eb3d09ef
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-04-23 00:06:09 +0900

    styled component 작성

---
+++ frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
+
+/**
+ * 회원가입 또는 로그인 폼을 보여줍니다.
+ */
+
+const AuthFormBlock = styled.div`
+    h3 {
+        margin: 0;
+        color: ${palette.gray[8]};
+        margin-bottom: 1rem;
+    }
+
+`;
+
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    )
+}
+
+export default AuthForm
