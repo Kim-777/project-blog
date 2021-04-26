504d414e6320a03293c5159e61df6f73526f53a9
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-04-27 08:27:51 +0900

    function: write

--- frontend/src/pages/WritePage.js
+++ frontend/src/pages/WritePage.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
+import Responsive from '../components/common/Responsive';
 
 const WritePage = () => {
     return (
-        <div>
-            글쓰기 페이지
-        </div>
+        <Responsive>
+            <EditorContainer />
+            <TagBoxContainer />
+            <WriteActionButtonsContainer />
+        </Responsive>
     )
 }
 
