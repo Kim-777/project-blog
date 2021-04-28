8462563c28cb3d3e065a21d1e3d42a6bb1acb343
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-04-28 15:21:32 +0900

    lib: quill

---
+++ frontend/src/components/write/Editor.js
@@ -0,0 +1,67 @@
+import React, { useRef, useEffect } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const EditorBlock = styled(Responsive)`
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+`;
+
+const TitleInput = styled.input`
+    font-size: 3rem;
+    outline: none;
+    padding-bottom: 0.5rem;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[4]};
+    margin-bottom: 2rem;
+    width:100%;
+`;
+
+const QuillWrapper = styled.div`
+    .ql-editor {
+        padding: 0;
+        min-height: 320px;
+        font-size: 1.125rem;
+        line-height: 1.5;
+    }
+    .ql-editor.ql-blank::before {
+        left: 0px;
+    }
+`;
+
+const Editor = () => {
+
+    const quillElement = useRef(null);
+    const quillInstance = useRef(null);
+
+    useEffect(() => {
+        quillInstance.current = new Quill(quillElement.current, {
+            theme: 'bubble',
+            placeholder: '내용을 작성하세요😎',
+            modules: {
+                // 더 많은 옵션
+                // https://quilljs.com/docs/modules/toolbar/ 참고
+                toolbar: [
+                    [{header: '1'}, {header: '2'}],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    [{list: 'ordered'}, {list: 'bullet'}],
+                    ['blockquote', 'code-block', 'link', 'image'],
+                ]
+            },
+        })
+    }, [])
+
+    return (
+        <EditorBlock>
+            <TitleInput placeholder="제목을 입력하세요" />
+            <QuillWrapper>
+                <div ref={quillElement} />
+            </QuillWrapper>
+        </EditorBlock>
+    )
+}
+
+export default Editor
