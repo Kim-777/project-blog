67a28efef40abb06ecfb29ed68209336ce936ce3
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-04-30 15:06:14 +0900

    front: modal

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 
 import api from './api';
@@ -39,13 +42,16 @@ app.use(jwtMiddleware);
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-router.get('/', ctx => {
-    ctx.body= {
-        head: 'header'
+const buildDirectory = path.resolve(__dirname, '../../frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+    // Not Found이고, 주소가 /api로 시작하지 않는 경우
+    if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+        // index.html 내용을 반환
+        await send(ctx, 'index.html', { root: buildDirectory});
     }
+});
 
-    return;
-})
 const port = PORT || 4000;
 app.listen(port, () => {
     console.log(`listening to port on ${port}`);
